897b9d2edd3056fb53a0086507321d464383805e
Diana <diana.narvaez@epn.edu.ec>
2019-04-16 19:51:47 -0500

    Ejercicio2

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -4,4 +4,22 @@
  *
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
 */
+
+<?php
+
+
+/*Ejercicio 2*/
+function titulo($titu)
+{
+  echo "<h1 style=\"text-align:center\">";
+  echo $titu;
+ 
+}
+
+titulo("Segundo Ejercicio ");
+
+titulo("Segundo Llamado");
+titulo("Holi");
+
+?>
 ?>
