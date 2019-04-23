9ed0c6bba082b7b57c5a965b0c76f4d05aa20876
desarrolloDiana <diana.narvaez@epn.edu.ec>
2019-04-23 09:43:48 -0500

    Ejercicio 6

--- ejercicio6/index.php
+++ ejercicio6/index.php
@@ -11,3 +11,20 @@ acuífero
 http://buscapalabras.com.ar/palabras-con-las-cinco-vocales.html
  */
 ?>
+
+
+<?php
+	$palabra = "Murcielago";
+	$Vocales = array ('a','e','i','o','u'); 
+	echo "Palabra o Frase es: ".$palabra."<br/>";
+	echo "<br>";
+	$miArray = count_chars (strtolower($palabra),1);
+
+	foreach($miArray as $indiceNum => $veces){
+		$letra =chr($indiceNum); 
+		if (in_Array($letra,$Vocales)){
+			echo "Vocal: ".$letra.", encontrada ".$veces."veces<br/>";
+		}
+	}
+	echo "<br>";
+?>
